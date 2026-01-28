b0d5f7a2ae91512ca4efefb383f5e866016bed6d
marcellk025-afk <marcellk025@gmail.com>
2026-01-28 10:47:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class VintedBot(discord.Client):
     async def on_ready(self):
         print(f"--- {self.user} ONLINE ÉS FIGYEL ---")
 
-    @tasks.loop(seconds=80)
+    @tasks.loop(seconds=100)
     async def monitor(self):
         channel = self.get_channel(CHANNEL_ID)
         if not channel or not TOKEN: return
